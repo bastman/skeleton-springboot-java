d382a5dd4effef0ef4f39507dd5f7f71b9f6710e
bastman <gtgames88>
2017-05-10 08:54:36 +0200

    add example tweets to repository on application start

--- rest-service/src/main/java/com/example/demo/restservice/DemoRestApplication.java
+++ rest-service/src/main/java/com/example/demo/restservice/DemoRestApplication.java
@@ -2,6 +2,8 @@ package com.example.demo.restservice;
 
 
 import com.example.demo.logging.AppLogger;
+import com.example.demo.restservice.domain.tweets.Tweet;
+import com.example.demo.restservice.domain.tweets.TweetService;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -9,6 +11,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+
+import java.time.Instant;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class DemoRestApplication {
@@ -24,10 +29,17 @@ public class DemoRestApplication {
     @Bean
     public CommandLineRunner init(
             ConfigurableApplicationContext ctx,
-            @Value("${app.appName}") String appName
+            @Value("${app.appName}") String appName,
+            TweetService tweetService
     ) {
         return (args) -> {
             LOGGER.info("=== init " + appName + " - " + ctx + " =====");
+
+            Arrays.asList(
+                    new Tweet("1", Instant.now(), "seb", "message 1"),
+                    new Tweet("2", Instant.now(), "seb", "message 2")
+            ).forEach(tweetService::submit);
+
         };
     }
 }
